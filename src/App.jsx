93f064b5e5b42a95f63f9954e10732913c33bab3
Yeison Estiven Perea Córdoba <ye.perea09@ciaf.edu.co>
2025-06-01 23:37:56 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  let [count, setCount] = useState(() => {
+    console.log("useEffect");
+    let savedCount = localStorage.getItem("count");
+    return savedCount !== null ? Number(savedCount) : 0;
+  });
+
+  function increment() {
+    setCount(count + 1);
+    console.log(count);
+  }
+
+  function decrement() {
+    if (count == 0) {
+      return;
+    }
+    setCount(count - 1);
+    console.log(count);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("count", count.toString());
+  }, [count]);
+
+  return (
+    <>
+      <div>
+        <h1>React Counter</h1>
+        <h2>{count}</h2>
+        <button onClick={() => increment()}>Increment</button>
+        <button onClick={() => decrement()}>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
